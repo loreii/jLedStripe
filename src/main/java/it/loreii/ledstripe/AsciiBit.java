ee599129da2d76d85b12611cf663e51662e10614
lpavesi <lpavesi@marchnetworks.com>
2017-02-23 09:53:30 +0100

    add utility class for map ascii chars to bitmap

---
+++ src/main/java/it/loreii/ledstripe/AsciiBit.java
@@ -0,0 +1,18 @@
+package it.loreii.ledstripe;
+
+/**
+ * Utility class for mapping ASCII chars to bitmap
+ * */
+public class AsciiBit {
+
+	char[] a = {
+			0b00000000,
+			0b01111000,
+			0b00000100,
+			0b01111100,
+			0b01000100,
+			0b00111110,
+			0b00000001,
+	}; 
+	
+}
